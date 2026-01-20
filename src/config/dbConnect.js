567e27f9b5d3f94f1d72c6f999495620f06a3c84
savii0414 <116351593+savii0414@users.noreply.github.com>
2026-01-20 13:22:08 +0530

    Initial commit - Secure Banking backend

---
+++ src/config/dbConnect.js
@@ -0,0 +1,15 @@
+import { connect } from "mongoose";
+
+const dbConnect = async() => {
+
+    try {
+        const mongoDbConnection = await connect(process.env.)
+        
+    } catch (error) {
+        console.log(`Database connection Failed ${error}`);
+        process.exit(1);
+        
+    }
+};
+
+export default dbConnect
