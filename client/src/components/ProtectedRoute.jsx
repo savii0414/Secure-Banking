aa8d8e6841ac63de4c3aae89d34709f780222d30
savii0414 <116351593+savii0414@users.noreply.github.com>
2026-02-08 16:58:15 +0530

    session

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -3,9 +3,13 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSession } from "../context/SessionContext";
 
 const ProtectedRoute = () => {
-  const { IsLoggedIn } = useSession();
+  const { IsLoggedIn, loading } = useSession();
 
   // If not logged in, redirect to login
+
+  if(loading){
+    return <div>Loading....</div>
+  }
   if (!IsLoggedIn) {
     return <Navigate to="/login" replace/>;
   }
--- client/src/context/SessionContext.jsx
+++ client/src/context/SessionContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const SessionContext = createContext();
 
@@ -7,19 +7,32 @@ export const useSession = () => useContext(SessionContext);
 
 export const SessionProvider = ({ children }) => {
   const [IsLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(sessionStorage.getItem("user"));
+    if(storedUser){
+      setUser(storedUser);
+      setIsLoggedIn(true);
+    }
+    setLoading(false);
+  },[]);
+
   const login = (userData) => {
     setIsLoggedIn(true);
     setUser(userData);
+    sessionStorage.setItem("user", JSON.stringify(userData));
   };
   const logout = (data) => {
     if (data) {
       setIsLoggedIn(false);
       setUser(null);
+      sessionStorage.removeItem("user");
     }
   };
   return (
-    <SessionContext.Provider value={{ IsLoggedIn, user, login, logout }}>
+    <SessionContext.Provider value={{ IsLoggedIn, loading, user, login, logout }}>
       {children}
     </SessionContext.Provider>
   );
