27f1b161e0eaf376a2c165c5035cfa2de9dcf5b9
savii0414 <116351593+savii0414@users.noreply.github.com>
2026-02-09 11:24:15 +0530

    Bug

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -35,6 +35,7 @@ app.use(cors({
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
+app.set("trust proxy", 1);
 // -----------------------------
 // Session Middleware
 // -----------------------------
